3742d38528cd79e0cd3d940310d0bb66c74e5000
yskang <kys061@gmail.com>
2018-03-02 15:31:40 +0900

    done until adding serializers in Image app

--- myangram/images/admin.py
+++ myangram/images/admin.py
@@ -1,15 +1,41 @@
 from django.contrib import admin
 from . import models
 
+
 @admin.register(models.Image)
 class ImageAdmin(admin.ModelAdmin):
-    pass
+
+    list_display = (
+        'created_at',
+        'updated_at',
+        'file',
+        'location',
+        'caption',
+        'creator',
+    )
+    search_fields = ('location', 'caption', 'creator__username',)
+    # autocomplete_fields = ('location',)
+
 
 @admin.register(models.Like)
 class LikeAdmin(admin.ModelAdmin):
-    pass
+
+    list_display = (
+        'created_at',
+        'updated_at',
+        'creator',
+        'image',
+    )
+
 
 @admin.register(models.Comment)
 class CommentAdmin(admin.ModelAdmin):
-    pass
+
+    list_display = (
+        'created_at',
+        'updated_at',
+        'message',
+        'creator',
+        'image',
+    )
 
--- myangram/images/models.py
+++ myangram/images/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from myangram.users import models as user_models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+
+@python_2_unicode_compatible
 class TimeStampedModel(models.Model):
-    
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -11,6 +13,7 @@ class TimeStampedModel(models.Model):
         abstract = True
 
 
+@python_2_unicode_compatible
 class Image(TimeStampedModel):
 
     file = models.ImageField()
@@ -18,14 +21,26 @@ class Image(TimeStampedModel):
     caption = models.TextField()
     creator = models.ForeignKey(user_models.User, null=True)
 
+    def __str__(self):
+        return 'loc: {}, cap: {}'.format(self.location, self.caption)
 
+
+@python_2_unicode_compatible
 class Comment(TimeStampedModel):
 
     message = models.TextField()
     creator = models.ForeignKey(user_models.User, null=True)
     image = models.ForeignKey(Image, null=True)
 
+    def __str__(self):
+        return 'who: {} - msg: {} at {}'.format(self.creator, self.message, self.created_at)
+
+
+@python_2_unicode_compatible
 class Like(TimeStampedModel):
-    
+
     creator = models.ForeignKey(user_models.User, null=True)
     image = models.ForeignKey(Image, null=True)
+
+    def __str__(self):
+        return 'who: {} - msg: {} at {}'.format(self.creator, self.image, self.created_at)
